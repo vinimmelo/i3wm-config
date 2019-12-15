6c496398ea6c10d1954e46bb6fcd4f2dd51b9b08
Vinicius Melo <viniciusdesk@icloud.com>
2019-12-15 10:35:29 -0300

    Remove specif configs and improve install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ echo
 echo "Folders Created"
 echo
 
+cp ~/.config/i3/config ~/.config/i3/config.bkp
+
+echo
+echo "Config copied to ~/.config/i3/config.bkp"
+echo
+
 cp config ~/.config/i3/
 cp i3blocks.conf ~/.config/i3blocks/
 
