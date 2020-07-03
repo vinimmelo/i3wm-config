a04ca89a43ae90e06babe3c1bbd07534c973779e
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-07-03 18:22:46 -0300

    Add icons to playerctl and more small configs

--- polybar/scripts/player-mpris-simple.sh
+++ polybar/scripts/player-mpris-simple.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo " ♫ $(playerctl metadata artist 2> /dev/null) - $(playerctl metadata title 2> /dev/null)"
+    echo " $(playerctl metadata artist 2> /dev/null) - $(playerctl metadata title 2> /dev/null)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " ♨ $(playerctl metadata artist 2> /dev/null) - $(playerctl metadata title 2> /dev/null)"
+    echo " $(playerctl metadata artist 2> /dev/null) - $(playerctl metadata title 2> /dev/null)"
 else
     echo ""
 fi
